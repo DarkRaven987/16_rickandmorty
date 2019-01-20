f2a311e4d7a8f97aaa1c5475834f0aa38bfaa4ab
DarkRaven987 <darkraven987@gmail.com>
2019-01-20 16:03:19 +0200

    Create Header and Character list components

---
+++ src/components/CharList/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CharList(){
+	return(
+		<div>
+			CharList
+		</div>	
+	);
+}
+
+export default CharList;
---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+function Header(){
+	return(
+		<div>
+			<h1>
+				THE RICK AND MORTY API
+			</h1>
+			<div>
+			<h2>
+				HEY, DID YOU EVERE WANT TO HOLD A TERRY FOLD?
+			</h2>
+			<h2>
+				I GOT ONE RIGHT HERE, GRAB MY TERRY FLAP     
+			</h2>
+			</div>
+		</div>	
+	);
+}
+
+export default Header;
