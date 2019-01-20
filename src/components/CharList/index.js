1be8b7d6027cce7a2e93d76c04599f26fe0e1c6d
DarkRaven987 <darkraven987@gmail.com>
2019-01-20 18:08:17 +0200

    Fetch data and import to list

--- src/components/CharList/index.js
+++ src/components/CharList/index.js
@@ -1,11 +1,49 @@
 import React from 'react';
 
-function CharList(){
-	return(
-		<div>
-			CharList
-		</div>	
-	);
-}
+import CharListElement from '../CharListElement';
+
+import './style.css';
+
+class CharList extends React.Component{
+
+	constructor(props){
+		super(props);
+
+		this.state = {
+			chars: []
+		}
+
+	}
+
+
+	createHeroElement = () => {
+		const {chars} = this.state;
+		if(chars.length){
+			return chars.map(hero => <CharListElement key={hero.id} hero={hero}/>);
+		}
+		return <div>No heroes yet</div>
+	}
+
+	render(){
+		console.log("render");
+		return(
+			<div className="Home__ShowcaseWrapper">
+				<div className="Home__ShowcaseInner">
+					{this.createHeroElement()}
+				</div>
+			</div>	
+		);
+	
+	}	
+
+	componentDidMount(){
+		fetch('https://rickandmortyapi.com/api/character/')
+		.then(res => res.json())
+		.then( res => {
+			this.setState({chars: res.results});
+		}
+		);
+	}
+};
 
 export default CharList;
---
+++ src/components/CharListElement/index.js
@@ -0,0 +1,45 @@
+import React, { Component, Fragment } from 'react';
+
+
+function CharListElement(props) {
+
+	const {id, status, image, name, species, gender,origin, location} = props.hero;
+
+  return (
+		<article className="CharacterCard__Wrapper">
+		<div data="card header" className="CharacterCard__ImgWrapper">
+			<div className="card-image">
+				<img src={image} alt="Davin"/>
+			</div>
+			<div className="CharacterCard__Title">
+				<h2 className="CharacterCard__Name">{name}</h2>
+				<p className="CharacterCard__Description">id: {id} - created a year ago</p>
+			</div>
+		</div>
+		<div data="card info" className="CharacterCard__InfoWrapper">
+			<div className="CharacterCard__TextWrapper">
+				<span>STATUS</span>
+				<p>{status}</p>
+			</div>
+			<div className="CharacterCard__TextWrapper">
+				<span>SPECIES</span>
+				<p>{species}</p>
+			</div>
+			<div className="CharacterCard__TextWrapper">
+				<span>GENDER</span>
+				<p>{gender}</p>
+			</div>
+			<div className="CharacterCard__TextWrapper">
+				<span>ORIGIN</span>
+				<p>{origin.name}</p>
+			</div>
+			<div className="CharacterCard__TextWrapper">
+				<span>LAST LOCATION</span>
+				<p>{location.name}</p>	
+			</div>
+		</div>
+	</article>
+    );
+}
+
+export default CharListElement;
